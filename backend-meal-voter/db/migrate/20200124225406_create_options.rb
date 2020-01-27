25239afc2c3d11fbc7302548e166f9e7b12b4f46
TonyB <this.tonybaker@gmail.com>
2020-01-27 12:00:11 -0800

    Used --api flag and created a new Rails application

---
+++ backend-meal-voter/db/migrate/20200124225406_create_options.rb
@@ -0,0 +1,11 @@
+class CreateOptions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :options do |t|
+      t.string :name
+      t.integer :votes
+      t.references :Meal, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
