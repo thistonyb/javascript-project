d6697a9c8b8c224e8921f479f83518d8e20054c9
TonyB <this.tonybaker@gmail.com>
2020-01-27 12:03:26 -0800

    Added some CRUD in controllers

---
+++ backend-meal-voter/app/controllers/meals_controller.rb
@@ -0,0 +1,30 @@
+class MealsController < ApplicationController
+
+    def show
+        meal = Meal.find(params[:id])
+        render json: MealSerializer.new(meal)
+    end
+
+    def index
+        meals = Meal.all
+        render json: MealSerializer.new(meals)
+    end
+
+    def create
+        meal = Meal.new(meal_params)
+        if meal.save
+            render json: MealSerializer.new(meal)
+        end
+    end
+
+    def destroy
+        meal = Meal.find(params[:id])
+        meal.delete
+        render json: MealSerializer.new(meal)
+    end
+
+    private
+        def meal_params
+            params.require(:meal).permit(:name, :date)
+        end
+end
---
+++ backend-meal-voter/app/controllers/options_controller.rb
@@ -0,0 +1,29 @@
+class OptionsController < ApplicationController
+    def show
+        option = Option.find(params[:id])
+        render json: OptionSerializer.new(option)
+    end
+
+    def index
+        options = Option.all
+        render json: OptionSerializer.new(options)
+    end
+
+    def create
+        option = Option.new(option_params)
+        if option.save
+            render json: OptionSerializer.new(option)
+        end
+    end
+
+    def destroy
+        option = Option.find(params[:id])
+        option.delete
+        render json: OptionSerializer.new(option)
+    end
+
+    private
+        def option_params
+            params.require(:option).permit(:name, :votes, :meal_id)
+        end
+end
