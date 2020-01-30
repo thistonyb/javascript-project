a75b2f3f068bb55a668042522cd8a538d96c4fbd
TonyB <this.tonybaker@gmail.com>
2020-01-30 12:57:22 -0800

    Create js file for a class that will do the fetching.
    URLs and Basic class and methods

---
+++ frontend-meal-voter/MealVoterApi.js
@@ -0,0 +1,10 @@
+const BASE_URL = "http://localhost:3000";
+const MEALS_URL = `${BASE_URL}/meals`;
+const OPTIONS_URL = `${BASE_URL}/options`;
+
+class MealVoterApi {
+  static get() {}
+  static post() {}
+  static update() {}
+  static delete() {}
+}
