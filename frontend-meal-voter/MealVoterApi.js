69cb3a50cf3a497cd1b9ca15891930828f652170
TonyB <this.tonybaker@gmail.com>
2020-01-31 09:55:11 -0800

    Make sure the html content loads first, create basic get/fetch

--- frontend-meal-voter/MealVoterApi.js
+++ frontend-meal-voter/MealVoterApi.js
@@ -1,9 +1,14 @@
 const BASE_URL = "http://localhost:3000";
 const MEALS_URL = `${BASE_URL}/meals`;
 const OPTIONS_URL = `${BASE_URL}/options`;
-
+//Call fetch on a URL and render JSON on a callback
 class MealVoterApi {
-  static get() {}
+  static get(url, render) {
+    fetch(url)
+      .then(response => response.json())
+      .then(json => render(json));
+  }
+
   static post() {}
   static update() {}
   static delete() {}
