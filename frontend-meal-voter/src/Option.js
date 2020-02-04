7253b79151565de3ea5cf774535ee207efb2d368
TonyB <this.tonybaker@gmail.com>
2020-02-04 14:12:57 -0800

    Created Option class with constructor and static parser method

---
+++ frontend-meal-voter/src/Option.js
@@ -0,0 +1,12 @@
+class Option {
+  constructor(id, name, votes, mealId) {
+    this.id = id;
+    this.name = name;
+    this.votes = votes;
+    this.meal_id = mealId;
+  }
+  //Takes json object and turns into Option object
+  static constructFromJson(json) {
+    return new Option(json.id, json.name, json.votes, json.meal_id);
+  }
+}
