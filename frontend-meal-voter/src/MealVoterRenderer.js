b10bb2c3c10515d680ff80a9aaef548aedd788da
TonyB <this.tonybaker@gmail.com>
2020-02-12 20:33:22 -0800

    Created static render options method

--- frontend-meal-voter/src/MealVoterRenderer.js
+++ frontend-meal-voter/src/MealVoterRenderer.js
@@ -1,7 +1,7 @@
 class MealVoterRenderer {
-  static renderMeals(obj) {
+  static renderMeals(mealObjArray) {
     const main = document.getElementsByTagName("main")[0];
-    for (const meal of obj) {
+    for (const meal of mealObjArray) {
       const mealCard = document.createElement("div");
       mealCard.setAttribute("class", "meal-card");
       mealCard.setAttribute("meal-id", `${meal.id}`);
@@ -22,6 +22,35 @@ class MealVoterRenderer {
       const optionsList = createElement("ul");
       optionsList.setAttribute("class", "options-list");
       mealCard.appendChild(optionsList);
+    }
+  }
+
+  static renderOptions(optionObjArray) {
+    const cards = document.getElementsByClassName("meal-card");
+    const idToUl = {};
+    for (const card of cards) {
+      const cardMealId = card.getAttribute("meal-id");
+      const ul = card.getElementsByTagName("ul")[0];
+      idToUl[cardMealId] = ul;
+    }
+    for (const option of optionObjArray) {
+      const li = document.createElement("li");
+      li.setAttribute("class", "option-row");
+      const optionName = document.createElement("div");
+      optionName.textContent = `${option.name}`;
+      li.appendChild(optionName);
+      const optionVotes = document.createElement("div");
+      optionVotes.textContent = `${option.votes}`;
+      li.appendChild(optionVotes);
+      const voteButton = document.createElement("button");
+      voteButton.setAttribute("option-id", `${option.id}`);
+      voteButton.setAttribute("class", "voteButton");
+      voteButton.textContent = "Vote!";
+      li.appendChild(voteButton);
+      const ul = idToUl[option.meal_id];
+      ul.appendChild(li);
+
+      //voteButton.addEventListener("click", onClickVote);
     }
   }
 }
