b29f434f0f33b30adbf106405135a2f9ccbe7005
TonyB <this.tonybaker@gmail.com>
2020-02-13 18:27:11 -0800

    Created render page method

--- frontend-meal-voter/src/MealVoterRenderer.js
+++ frontend-meal-voter/src/MealVoterRenderer.js
@@ -1,14 +1,45 @@
 class MealVoterRenderer {
+  static renderPage() {
+    const main = document.getElementsByTagName("main")[0];
+    const addMealBanner = document.createElement("form");
+    addMealBanner.setAttribute("class", "add-meal-banner");
+    main.appendChild(addMealBanner);
+    const nameInput = document.createElement("input");
+    nameInput.setAttribute("type", "text");
+    nameInput.setAttribute("name", "name");
+    nameInput.setAttribute("value", "");
+    nameInput.setAttribute("placeholder", "Meal Name");
+    nameInput.setAttribute("class", "name-input");
+    addMealBanner.appendChild(nameInput);
+    const dateInput = document.createElement("input");
+    dateInput.setAttribute("type", "text");
+    dateInput.setAttribute("name", "date");
+    dateInput.setAttribute("value", "");
+    dateInput.setAttribute("placeholder", "Meal Date");
+    dateInput.setAttribute("class", "date-input");
+    addMealBanner.appendChild(dateInput);
+    const submitButton = document.createElement("input");
+    submitButton.setAttribute("type", "submit");
+    submitButton.setAttribute("name", "submit");
+    submitButton.setAttribute("value", "Create a Meal");
+    submitButton.setAttribute("class", "submit-button");
+    addMealBanner.appendChild(submitButton);
+    // submitButton.addEventListener("click", )
+
+    const mealCards = document.createElement("div");
+    mealCards.setAttribute("class", "meal-cards");
+    main.appendChild(mealCards);
+  }
   /**Takes in an array of parsed json objects that are now Meal objects.
    * Renders each Meal as a div and sets up a button to add Options.
    */
   static renderMeals(mealObjArray) {
-    const main = document.getElementsByTagName("main")[0];
+    const mealCards = document.getElementsByClassName("meal-cards")[0];
     for (const meal of mealObjArray) {
       const mealCard = document.createElement("div");
       mealCard.setAttribute("class", "meal-card");
       mealCard.setAttribute("meal-id", `${meal.id}`);
-      main.appendChild(mealCard);
+      mealCards.appendChild(mealCard);
       const mealName = document.createElement("div");
       mealName.setAttribute("class", "meal-name");
       mealName.textContent = `${meal.name}`;
@@ -17,14 +48,14 @@ class MealVoterRenderer {
       mealDate.setAttribute("class", "meal-date");
       mealDate.textContent = `${meal.date}`;
       mealCard.appendChild(mealDate);
+      const optionsList = createElement("ul");
+      optionsList.setAttribute("class", "options-list");
+      mealCard.appendChild(optionsList);
       const addButton = document.createElement("button");
       addButton.setAttribute("class", "add-option-button");
       addButton.textContent = "+ Meal Option";
       // addButton.addEventListener("click", onClickAddOption);
       mealCard.appendChild(addButton);
-      const optionsList = createElement("ul");
-      optionsList.setAttribute("class", "options-list");
-      mealCard.appendChild(optionsList);
     }
   }
   /**Takes in an array of parsed json objects that are now Option objects.
@@ -59,5 +90,8 @@ class MealVoterRenderer {
     }
   }
 
-  static onClickAddOption(event) {}
+  static onClickAddOption(event) {
+    const addButton = event.currentTarget;
+    const optionsList = addButton.parentElement.getElementsByClassName;
+  }
 }
