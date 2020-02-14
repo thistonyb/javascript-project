b35a41cf2b58f133edc88c9954d6fd75fd1f0f7d
TonyB <this.tonybaker@gmail.com>
2020-02-14 14:55:40 -0800

    Added comment on how Options gets rendered second

--- frontend-meal-voter/src/index.js
+++ frontend-meal-voter/src/index.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
   MealVoterRenderer.renderPage();
   MealVoterApi.getMeals(getMealsCallback);
 });
+
 /**Making sure the Options are renderend after the Meals by utilizing
  * the getMeals callback */
-
 function getMealsCallback(meals) {
   MealVoterRenderer.renderMeals(meals);
   MealVoterApi.getOptions(MealVoterRenderer.renderOptions);
