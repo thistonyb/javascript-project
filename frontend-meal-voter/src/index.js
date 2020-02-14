abc1d1aad4dfffcb05c2fa7e5db933e746dfc976
TonyB <this.tonybaker@gmail.com>
2020-02-14 14:04:55 -0800

    Made calls to the renderPage, getMeals and getOptions

--- frontend-meal-voter/src/index.js
+++ frontend-meal-voter/src/index.js
@@ -1,1 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+  MealVoterRenderer.renderPage();
+  MealVoterApi.getMeals(getMealsCallback);
+});
+/**Making sure the Options are renderend after the Meals by utilizing
+ * the getMeals callback */
+
+function getMealsCallback(meals) {
+  MealVoterRenderer.renderMeals(meals);
+  MealVoterApi.getOptions(MealVoterRenderer.renderOptions);
+}
