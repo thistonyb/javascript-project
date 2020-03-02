2119585b33741f73cc514d6bbc3126d7956c471c
TonyB <this.tonybaker@gmail.com>
2020-03-01 18:36:07 -0800

    Meal constructor takes in options and json gets parsed

--- frontend-meal-voter/src/Meal.js
+++ frontend-meal-voter/src/Meal.js
@@ -1,11 +1,21 @@
 class Meal {
-  constructor(id, name, date) {
+  constructor(id, name, date, options) {
     this.id = id;
     this.name = name;
     this.date = date;
+    this.options = options;
   }
   //Takes a json object and turns into Meal object
   static constructFromJson(json) {
-    return new Meal(json.id, json.name, json.date);
+    const options = [];
+    if (json.options) {
+      for (const option of json.options) {
+        options.push(
+          new Option(option.id, option.name, option.votes, option.meal_id)
+        );
+      }
+    }
+
+    return new Meal(json.id, json.name, json.date, options);
   }
 }
